94a4dce31818052537ec0d3906b12bc490b8a4b7
Ariel Juodziukynas <arieljuod@gmail.com>
2025-08-25 12:18:55 -0300

    Patch Jsbundling and execjs commands with relateive bun binary

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,24 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Use relative standalone bun binary when running the assets:precompile task
+module Jsbundling
+  module Tasks
+    def install_command
+      "./bun install"
+    end
+
+    def build_command
+      "./bun run build"
+    end
+  end
+end
+
+# Fix uglifier error looking for bun system-wide
+ExecJS::Runtimes::Bun = ExecJS::ExternalRuntime.new(
+  name: "Bun.sh",
+  command: [ "./bun" ],
+  runner_path: ExecJS.root + "/support/bun_runner.js",
+  encoding: "UTF-8"
+)
